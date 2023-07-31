e636472cb83510cfb97ca4acde4eeddb3518eb5c
cheol <qjel60@naver.com>
2023-07-31 22:37:31 +0900

    fix:hardhatconfig.ts

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -1,5 +1,5 @@
-import { HardhatUserConfig } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox"
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.9",
@@ -7,22 +7,29 @@ const config: HardhatUserConfig = {
         hardhat: {
             chainId: 1337,
             accounts: {
-                mnemonic: 'test test test test test test test test test test test junk',
-              },
+                mnemonic: "test test test test test test test test test test test junk",
+            },
         },
         goerli: {
             url: "https://goerli.infura.io/v3/a489405bf8564256a8d2e3f99e1c1403",
-            accounts : require("./accounts.json").privateKey,
+            accounts: require("./accounts.json").privateKey,
             chainId: 5,
-            gas:20000000,
-            gasPrice: 25000000000
+            gas: 20000000,
+            gasPrice: 25000000000,
         },
         sepolia: {
             url: "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
             accounts: ["cb27d8183c54f7fbcece11313d75274048a4d8f54767db9a23ae294599cce431"],
             chainId: 11155111,
         },
+        cronos: {
+            url: "https://evm-t3.cronos.org/",
+            chainId: 338,
+            accounts: require("./accounts.json").privateKey,
+            gasPrice: 5000000000000,
+        },
     },
-}
+};
 
-export default config
+export default config;
+
