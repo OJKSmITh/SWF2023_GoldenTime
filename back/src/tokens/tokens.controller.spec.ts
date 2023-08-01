88cd1aafac212c07da34da01cb075ecb09522b5c
cloudcoke <lms4296@gmail.com>
2023-08-01 09:54:04 +0900

    Test : Back 배포 테스트

--- back/src/tokens/tokens.controller.spec.ts
+++ back/src/tokens/tokens.controller.spec.ts
@@ -1,43 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokensController } from './tokens.controller';
 import { TokensService } from './tokens.service';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { getModelToken } from '@nestjs/sequelize';
 import { Token } from './model/token.model';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import configuraction from '../config/configuraction';
+
+const mockTokenModel = () => ({
+  findAll: jest.fn(),
+});
 
 describe('TokensController', () => {
   let controller: TokensController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          cache: true,
-          isGlobal: true,
-          load: [configuraction],
-        }),
-        SequelizeModule.forRootAsync({
-          inject: [ConfigService],
-          useFactory: (config: ConfigService) => {
-            const mode = process.env.MODE;
-            const { db } = config.get(mode);
-            return {
-              dialect: db.dialect,
-              host: db.host,
-              port: db.port,
-              username: db.username,
-              password: db.password,
-              database: db.database,
-              autoLoadModels: db.autoLoadModels,
-              synchronize: db.synchronize,
-            };
-          },
-        }),
-        SequelizeModule.forFeature([Token]),
-      ],
       controllers: [TokensController],
-      providers: [TokensService],
+      providers: [
+        TokensService,
+        {
+          provide: getModelToken(Token),
+          useFactory: mockTokenModel,
+        },
+      ],
     }).compile();
 
     controller = module.get<TokensController>(TokensController);
